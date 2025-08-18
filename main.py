2cdab29e7673ea36e9a7bb3be2f958d9b932ccb6
thisdev-davi <davisouzaoliveira1975@gmail.com>
2025-08-18 00:52:09 -0300

    new stats.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
+from stats import count_words
+
 def get_book_file(path):
     with open(path, encoding="utf-8") as f:
         file_contents = f.read()
     return file_contents
 
-def count_words_file(file):
-    num_words = len(file.split())
-    print(f"{num_words} words found in the document")
-
 def main():
-    count_words_file(get_book_file("books/frankenstein.txt"))
+    count_words(get_book_file("books/frankenstein.txt"))
 
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def count_words(file):
+    num_words = len(file.split())
+    print(f"{num_words} words found in the document")
