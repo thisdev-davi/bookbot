2148cb9e083ba554b89f498e9396558a0b7bc2f9
thisdev-davi <davisouzaoliveira1975@gmail.com>
2025-08-20 11:01:26 -0300

    sys args

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from stats import get_characters, get_num_words, ordered_list
+import sys
 
 def get_book_file(path):
     with open(path, encoding="utf-8") as f:
         return f.read()
 
 def main():
-    path = "books/frankenstein.txt"
+    if(len(sys.argv) !=2 ):
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path = sys.argv[1]
     file = get_book_file(path)
     num_words = get_num_words(file)
     char_dict = get_characters(path)
     list_char = ordered_list(char_dict)
-
 
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {path}...")
